206e3f271636b68920edfc1c835ce2a7b0890c80
DilerCiftci <diler-ciftci-94@hotmail.com>
2026-02-15 03:47:59 +0100

    Created spring project

---
+++ iquest/src/main/java/com/diler/iquest/IquestApplication.java
@@ -0,0 +1,13 @@
+package com.diler.iquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IquestApplication.class, args);
+	}
+
+}
---
+++ iquest/src/test/java/com/diler/iquest/IquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diler.iquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
