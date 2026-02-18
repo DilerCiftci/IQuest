71911dc10414fba7d7a53151767928bbbc444582
DilerCiftci <diler-ciftci-94@hotmail.com>
2026-02-18 14:36:00 +0100

    Added db config, security service/config

---
+++ iquest/src/main/java/com/diler/iquest/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.diler.iquest.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.diler.iquest.model.User;
+import com.diler.iquest.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole() != null ? user.getRole() : "USER")
+                .build();
+    }
+
+}
